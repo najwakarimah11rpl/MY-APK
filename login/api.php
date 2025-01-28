745716589d72b53852e9bbfc4be162bb7f699bf8
nanaa <147923660+najwakarimah11rpl@users.noreply.github.com>
2025-01-28 22:42:32 +0700

    Delete login directory

--- login/api.php
+++
@@ -1,7 +0,0 @@
-<!-- logout.php -->
-<?php
-session_start(); // Mulai sesi
-session_destroy(); // Menghapus sesi
-header("Location: login.php"); // Arahkan kembali ke halaman login
-exit();
-?>
--- login/logout.php
+++
@@ -1,8 +0,0 @@
-<!-- logout.php -->
-<?php
-// Menghapus sesi login dan mengarahkan kembali ke halaman login
-session_start();
-session_destroy();
-header("Location: api.php"); // ganti index.php dengan halaman login Anda
-exit();
-?>
